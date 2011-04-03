e8590843edaa366d6c8bdb7d7be03895194168d1
kthakore <thakore.kartik@gmail.com>
2011-04-03 13:28:12 -0400

    Added mising files

---
+++ BallController.h
@@ -0,0 +1,38 @@
+#ifndef GAMECONTROLLER_H
+#define GAMECONTROLLER_H
+/*
+Class: BallController
+Purpose: Control and apply the game loop physics. 
+*/
+
+#include "Physics.h"
+
+class BallController
+{
+    public:
+		//Initiate one
+        BallController();
+        virtual ~BallController();
+
+		//Call once per glut display
+		void update();
+ 
+    private:
+		//Interporlates between states and the alpha value of the time changes
+		State interpolate( const State &previous, const State &current, float alpha );
+		//The acceleration of the ball
+		Vector acceleration( const State &state, float t);
+		//Evaluates the derivatives 
+		Derivative evaluate( const State &initial, float t);
+		//Overloads Evaluate 
+		Derivative evaluate( const State &initial, float t, float dt, const Derivative &d);
+		//Integrate the position and velocity using rungekutta
+		void integrate(State &state, float t, float dt);
+
+		//Time Attributes
+		float t; float dt; float currentTime; float accumulator;
+
+		State current; State previous;
+};
+
+#endif // GAMECONTROLLER_H
