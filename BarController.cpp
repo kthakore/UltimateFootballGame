7bb10035ba07f36187df30d47faf0a61d798e150
kthakore <thakore.kartik@gmail.com>
2011-04-03 23:15:07 -0400

    BarController working now

--- BarController.cpp
+++ BarController.cpp
@@ -2,13 +2,26 @@
 
 BarController::BarController( float min, float max, float initial_val, float increment)
 {
-
-
+	//nothing too complicated here, we are simply creating the BarController object
+	//the parameters passed will be used to set the initial parameters for this object
+	//this includes min value, max value, initial value, and increment value
+	this->min = min;
+	this->max = max;
+	this->val = initial_val;
+	this->increment = increment;
 }
 
 void BarController::update()
 {
-
+	//since the bar will be constantly updating, this function is essential
+	//we need to update the current information of the bar
+	this->val += this->increment;	//increment the total power by the speed
+        
+	if(this->val >= this->max || this->val <= this->min)	//the end values
+    {
+		//when you reach one end, the speed needs to flip so you can go back to the other end
+		this->increment *= -1;// switch between -1 and 1
+    }
 
 }
 
--- GameController.cpp
+++ GameController.cpp
@@ -1,5 +1,5 @@
 #include "GameController.h"
-
+#include <stdlib.h>
 
 #include "Physics.h"
 #include "BarController.h"
@@ -27,8 +27,8 @@ GameController::GameController()
 {
 
 	//Do this based on player attributes 
-	this->kick_angle = new BarController( -10.0, 10.0, 0.0, 1.0);
-	this->kick_power = new BarController( -10.0, 10.0, 0.0, 1.0);
+	this->kick_angle = new BarController( -0.01, 0.01, 0.0, 0.001);
+	this->kick_power = new BarController( 0.0, 0.04, 0.0, 0.01);
 
 	this->state = 'g'; //Just started so just go to game. We don't have a menu yet
 	this->goals = 0; this->misses = 0; this->chances_left = 0; 
@@ -49,6 +49,7 @@ void GameController::update()
 
 
 	this->kick_power->update();
+	
 	this->kick_angle->update();
 
 	this->ball.update();
@@ -69,8 +70,8 @@ void GameController::handle_key( unsigned char key, int x, int y )
 				case ' ':
 					//TODO: use the stuff from the bars kick_angle and kick_power
 					float angle =  randDouble( -0.000150, 0.000150) ;
-					Vector initial_velocity = Vector(randDouble( -0.000150, 0.000150),0.02 + randDouble( 0, 0.05) ,-( 0.05+randDouble(0.0,0.04) ) );
-		//			initial_velocity.debug("Kick initial velocity");
+					Vector initial_velocity = Vector( 0, 0.02 + this->kick_power->value() , - 0.05  -this->kick_power->value() );
+					initial_velocity.debug("Kick initial velocity");
 					Vector wind_affect_angle = Vector ( randDouble( -0.000150, 0.000150), 0.0000001, randDouble(-0.0001,0.0001));
 		//			wind_affect_angle.debug("Affect of Wind and Kick Angle");
 					this->ball.kick(  initial_velocity, wind_affect_angle );
